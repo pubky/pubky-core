5b2bfc878f6fcb600e58edd4edecf2cf77724bb8
nazeh <ar.nazeh@gmail.com>
2023-12-11 17:52:02 +0300

    feat: error from Io errors

--- kytz/src/error.rs
+++ kytz/src/error.rs
@@ -9,4 +9,8 @@ pub enum Error {
     /// For starter, to remove as code matures.
     #[error("Generic error: {0}")]
     Generic(String),
+
+    #[error(transparent)]
+    /// Error from `std::io::Error`.
+    Io(#[from] std::io::Error),
 }
