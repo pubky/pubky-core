c12c8797d19c8c0dab5a9cb1eaf6bcc006f5142b
nazeh <ar.nazeh@gmail.com>
2024-07-12 18:10:38 +0300

    chore: remove kytz package

--- kytz/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// #![allow(unused)]
-pub mod crypto;
-pub mod error;
-
-// Re-exports
-pub use error::Error;
-
-// Alias Result to be the crate Result.
-pub type Result<T, E = Error> = core::result::Result<T, E>;
