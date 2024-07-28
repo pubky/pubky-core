dac22840658d217534a86e01b40957c3d5b5e92f
nazeh <ar.nazeh@gmail.com>
2024-07-28 12:22:20 +0300

    feat(homeserver): add in memory pkarr relay api for testing

--- pubky-homeserver/src/main.rs
+++ pubky-homeserver/src/main.rs
@@ -1,13 +1,36 @@
 use anyhow::Result;
+use pkarr::mainline::Testnet;
 use pubky_homeserver::Homeserver;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// [tracing_subscriber::EnvFilter]
+    #[clap(short, long)]
+    tracing_env_filter: Option<String>,
+    #[clap(long)]
+    testnet: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Cli::parse();
+
     tracing_subscriber::fmt()
-        .with_env_filter("pubky_homeserver=debug,tower_http=debug")
+        .with_env_filter(
+            args.tracing_env_filter
+                .unwrap_or("pubky_homeserver=debug,tower_http=debug".to_string()),
+        )
         .init();
 
-    let server = Homeserver::start(Default::default()).await?;
+    let server = if args.testnet {
+        let testnet = Testnet::new(3);
+
+        Homeserver::start_test(&testnet).await?
+    } else {
+        Homeserver::start(Default::default()).await?
+    };
 
     server.run_until_done().await?;
 
