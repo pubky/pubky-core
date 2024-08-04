32fc7e7d6451c887a933cc8e727e6cf81d63058a
nazeh <ar.nazeh@gmail.com>
2024-08-04 14:18:35 +0300

    feat(js): rename from_secret_key to fromSecretKey

--- pubky/pkg/test/keys.js
+++ pubky/pkg/test/keys.js
@@ -5,7 +5,7 @@ import { Keypair } from '../index.cjs'
 test('generate keys from a seed', async (t) => {
   const secretkey = Buffer.from('5aa93b299a343aa2691739771f2b5b85e740ca14c685793d67870f88fa89dc51', 'hex')
 
-  const keypair = Keypair.from_secret_key(secretkey)
+  const keypair = Keypair.fromSecretKey(secretkey)
 
   const publicKey = keypair.publicKey()
 
