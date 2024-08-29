4222153b831cbd63edab5631b9267bbb219f07c6
nazeh <ar.nazeh@gmail.com>
2024-08-29 19:05:36 +0300

    examples(authz): start 3rd party app frontend

---
+++ examples/authz/3rd-party-app/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  publicDir: 'public',
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [
+    eslint({ cache: false }),
+  ],
+});
