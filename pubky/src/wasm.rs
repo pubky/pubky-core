915ae66f1a22c49a101fbc28a563adc6428653ca
nazeh <ar.nazeh@gmail.com>
2024-09-01 16:47:22 +0300

    feat(js): add window.pubky and move static methods out of PubkyClient

--- pubky/src/wasm.rs
+++ pubky/src/wasm.rs
@@ -6,13 +6,12 @@ use std::{
 use js_sys::{Array, Uint8Array};
 use wasm_bindgen::prelude::{wasm_bindgen, JsValue};
 
-use pubky_common::recovery_file::{create_recovery_file, decrypt_recovery_file};
-
-use crate::{error::Error, PubkyClient};
+use crate::PubkyClient;
 
 mod http;
 mod keys;
 mod pkarr;
+mod recovery_file;
 mod session;
 
 use keys::{Keypair, PublicKey};
@@ -47,30 +46,6 @@ impl PubkyClient {
             session_cookies: Arc::new(RwLock::new(HashSet::new())),
             pkarr_relays: TESTNET_RELAYS.into_iter().map(|s| s.to_string()).collect(),
         }
-    }
-
-    /// Create a recovery file of the `keypair`, containing the secret key encrypted
-    /// using the `passphrase`.
-    #[wasm_bindgen(js_name = "createRecoveryFile")]
-    pub fn create_recovery_file(
-        keypair: &Keypair,
-        passphrase: &str,
-    ) -> Result<Uint8Array, JsValue> {
-        create_recovery_file(keypair.as_inner(), passphrase)
-            .map(|b| b.as_slice().into())
-            .map_err(|e| Error::from(e).into())
-    }
-
-    /// Create a recovery file of the `keypair`, containing the secret key encrypted
-    /// using the `passphrase`.
-    #[wasm_bindgen(js_name = "decryptRecoveryFile")]
-    pub fn decrypt_recovery_file(
-        recovery_file: &[u8],
-        passphrase: &str,
-    ) -> Result<Keypair, JsValue> {
-        decrypt_recovery_file(recovery_file, passphrase)
-            .map(Keypair::from)
-            .map_err(|e| Error::from(e).into())
     }
 
     /// Set Pkarr relays used for publishing and resolving Pkarr packets.
