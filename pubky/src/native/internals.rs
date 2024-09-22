9fd85017182f217549a1c6dda6ecc36043388e41
nazeh <ar.nazeh@gmail.com>
2024-09-22 19:12:07 +0300

    refactor(pubky): simplify internals mod after using pkarr client

--- pubky/src/native/internals.rs
+++ pubky/src/native/internals.rs
@@ -1,28 +1,12 @@
-use pkarr::SignedPacket;
-use pubky_common::crypto::PublicKey;
 use reqwest::RequestBuilder;
 use url::Url;
 
-use crate::error::Result;
 use crate::PubkyClient;
 
 mod endpoints;
 pub mod resolver;
 
 impl PubkyClient {
-    // === Pkarr ===
-
-    pub(crate) async fn pkarr_resolve(
-        &self,
-        public_key: &PublicKey,
-    ) -> Result<Option<SignedPacket>> {
-        Ok(self.pkarr.resolve(public_key).await?)
-    }
-
-    pub(crate) async fn pkarr_publish(&self, signed_packet: &SignedPacket) -> Result<()> {
-        Ok(self.pkarr.publish(signed_packet).await?)
-    }
-
     // === HTTP ===
 
     pub(crate) fn inner_request(&self, method: reqwest::Method, url: Url) -> RequestBuilder {
