da2cc2a344a44c10f8686c600cdf63c433e9be13
nazeh <ar.nazeh@gmail.com>
2024-12-03 16:37:55 +0300

    feat(homeserver): add http server as well as https

--- pubky-homeserver/src/io/http.rs
+++ pubky-homeserver/src/io/http.rs
@@ -13,22 +13,76 @@ use axum_server::{
 
 use crate::core::HomeserverCore;
 
-pub(crate) async fn start(core: HomeserverCore) -> Result<Handle> {
-    let listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], core.config.port())))?;
+#[derive(Debug)]
+pub struct HttpServers {
+    /// Handle for the HTTP server
+    pub(crate) http_handle: Handle,
+    /// Handle for the HTTPS server using Pkarr TLS
+    pub(crate) https_handle: Handle,
+    // /// Handle for a mock relay used in testnet
+    // pub(crate) mock_pkarr_relay_handle: Handle,
+}
 
-    let acceptor = RustlsAcceptor::new(RustlsConfig::from_config(Arc::new(
-        core.keypair().to_rpk_rustls_server_config(),
-    )));
-    let server = axum_server::from_tcp(listener).acceptor(acceptor);
+impl HttpServers {
+    pub async fn start(core: &HomeserverCore) -> Result<Self> {
+        let http_listener =
+        // TODO: add config to http port
+            TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], 0)))?;
 
-    let handle = Handle::new();
+        let http_handle = Handle::new();
 
-    tokio::spawn(
-        server.handle(handle.clone()).serve(
-            core.router
-                .into_make_service_with_connect_info::<SocketAddr>(),
-        ),
-    );
+        tokio::spawn(
+            axum_server::from_tcp(http_listener)
+                .handle(http_handle.clone())
+                .serve(
+                    core.router
+                        .clone()
+                        .into_make_service_with_connect_info::<SocketAddr>(),
+                ),
+        );
 
-    Ok(handle)
+        let https_listener =
+            TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], core.config.port())))?;
+
+        let https_handle = Handle::new();
+
+        tokio::spawn(
+            axum_server::from_tcp(https_listener)
+                .acceptor(RustlsAcceptor::new(RustlsConfig::from_config(Arc::new(
+                    core.keypair().to_rpk_rustls_server_config(),
+                ))))
+                .handle(https_handle.clone())
+                .serve(
+                    core.router
+                        .clone()
+                        .into_make_service_with_connect_info::<SocketAddr>(),
+                ),
+        );
+
+        // let mock_pkarr_relay_listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], 15411)))?;
+
+        Ok(Self {
+            http_handle,
+            https_handle,
+        })
+    }
+
+    pub async fn http_address(&self) -> Result<SocketAddr> {
+        match self.http_handle.listening().await {
+            Some(addr) => Ok(addr),
+            None => Err(anyhow::anyhow!("Failed to bind to http port")),
+        }
+    }
+
+    pub async fn https_address(&self) -> Result<SocketAddr> {
+        match self.https_handle.listening().await {
+            Some(addr) => Ok(addr),
+            None => Err(anyhow::anyhow!("Failed to bind to https port")),
+        }
+    }
+
+    pub fn shutdown(&self) {
+        self.http_handle.shutdown();
+        self.https_handle.shutdown();
+    }
 }
