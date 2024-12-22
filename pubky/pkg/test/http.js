ea79500b6077932d2986ef97cb43383dc701f9e2
nazeh <ar.nazeh@gmail.com>
2024-12-22 15:41:00 +0300

    feat(js): pass all unit tests

--- pubky/pkg/test/http.js
+++ pubky/pkg/test/http.js
@@ -4,22 +4,38 @@ import { Client, Keypair, PublicKey } from '../index.cjs'
 
 const TLD = '8pinxxgqs41n4aididenw5apqp1urfmzdztr8jt4abrkdn435ewo';
 
-// TODO: test HTTPs too somehow.
-
 test("basic fetch", async (t) => {
   let client = Client.testnet();
 
   // Normal TLD
   {
-    let response = await client.fetch(`http://relay.pkarr.org/`);
+    let response = await client.fetch(`https://relay.pkarr.org/`);
 
     t.equal(response.status, 200);
   }
 
 
   // Pubky
-  let response = await client.fetch(`http://${TLD}/`);
+  let response = await client.fetch(`https://${TLD}/`);
 
   t.equal(response.status, 200);
+})
+
+test("fetch failed", async (t) => {
+
+  let client = Client.testnet();
+
+  // Normal TLD
+  {
+    let response = await client.fetch(`https://nonexistent.domain/`).catch(e => e);
+
+    t.ok(response instanceof Error);
+  }
+
+
+  // Pubky
+  let response = await client.fetch(`https://1pinxxgqs41n4aididenw5apqp1urfmzdztr8jt4abrkdn435ew1/`).catch(e => e);
+
+  t.ok(response instanceof Error);
 })
 
