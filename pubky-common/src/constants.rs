a14f8008b890ea9767d931c762fa4502382170fe
nazeh <ar.nazeh@gmail.com>
2025-01-08 13:37:37 +0300

    feat: use reserved HTTP_PORT service param for testnet

---
+++ pubky-common/src/constants.rs
@@ -0,0 +1,9 @@
+/// [Reserved param keys](https://www.rfc-editor.org/rfc/rfc9460#name-initial-contents) for HTTPS Resource Records
+pub mod reserved_param_keys {
+    pub const HTTP_PORT: u16 = 65280;
+}
+
+pub mod testnet_ports {
+    pub const PKARR_RELAY: u16 = 15411;
+    pub const HTTP_RELAY: u16 = 15412;
+}
