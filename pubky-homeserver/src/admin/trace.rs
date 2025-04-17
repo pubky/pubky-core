793299c8da9cb78883c13a0aecda5f81cf028a7f
Severin Buhler <sevi_buehler@hotmail.com>
2025-04-17 14:41:20 +0200

    cleaned up admin trace

--- pubky-homeserver/src/admin/trace.rs
+++ pubky-homeserver/src/admin/trace.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 use axum::{extract::Request, Router};
 use tower_http::trace::{
     DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, OnFailure, OnRequest, OnResponse,
@@ -7,48 +5,26 @@ use tower_http::trace::{
 };
 use tracing::{Level, Span};
 
-pub fn with_trace_layer(router: Router, excluded_paths: &[&str]) -> Router {
-    let excluded_paths = Arc::new(
-        excluded_paths
-            .iter()
-            .map(|s| s.to_string())
-            .collect::<Vec<_>>(),
-    );
+pub fn with_trace_layer(router: Router) -> Router {
 
     router.layer(
         TraceLayer::new_for_http()
             .make_span_with(move |request: &Request| {
-                if excluded_paths.contains(&request.uri().path().to_string()) {
-                    // Skip logging for the noisy endpoint
-                    tracing::span!(Level::INFO, "request", excluded = true)
-                } else {
-                    // Use the default span for other endpoints
-
-                    let uri = request.uri().to_string();
-
-                    tracing::span!(
-                        Level::INFO,
-                        "request",
-                        method = %request.method(),
-                        uri = ?uri,
-                        version = ?request.version(),
-                    )
-                }
+                let uri = request.uri().to_string();
+                tracing::span!(
+                    Level::INFO,
+                    "request",
+                    method = %request.method(),
+                    uri = ?uri,
+                    version = ?request.version(),
+                )
             })
             .on_request(|request: &Request, span: &Span| {
-                // Skip logging for excluded spans
-                if span.has_field("excluded") {
-                    return;
-                }
                 // Use the default behavior for other spans
                 DefaultOnRequest::new().on_request(request, span);
             })
             .on_response(
                 |response: &axum::response::Response, latency: std::time::Duration, span: &Span| {
-                    // Skip logging for excluded spans
-                    if span.has_field("excluded") {
-                        return;
-                    }
                     // Use the default behavior for other spans
                     DefaultOnResponse::new().on_response(response, latency, span);
                 },
@@ -57,10 +33,6 @@ pub fn with_trace_layer(router: Router, excluded_paths: &[&str]) -> Router {
                 |error: tower_http::classify::ServerErrorsFailureClass,
                  latency: std::time::Duration,
                  span: &Span| {
-                    // Skip logging for excluded spans
-                    if span.has_field("excluded") {
-                        return;
-                    }
                     // Use the default behavior for other spans
                     DefaultOnFailure::new().on_failure(error, latency, span);
                 },
