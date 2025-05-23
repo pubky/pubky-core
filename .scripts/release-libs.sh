0d6b9abc29fece2f6a8cfbb95833b158cfb44ae3
Severin Buhler <sevi_buehler@hotmail.com>
2025-05-23 10:05:26 +0200

    workspaces check

--- .scripts/release-libs.sh
+++ .scripts/release-libs.sh
@@ -8,6 +8,14 @@ set -e # fail the script if any command fails
 set -u # fail the script if any variable is not set
 set -o pipefail # fail the script if any pipe command fails
 
+# Check if cargo-set-version is installed
+if ! cargo --list | grep -q "workspaces"; then
+  echo "Error: cargo-workspaces is not installed but required."
+  echo "Please install it first by running:"
+  echo "  cargo install cargo-workspaces"
+  exit 1
+fi
+
 
 # Publish all the crates of the workspace to crates.io.
 # ws does this in the correct order on how crates are depended on each other.
