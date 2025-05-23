3229f0a2371493bb072ee67bfd7f5f52f34ec25f
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2025-05-23 13:17:42 +0200

    test rate limit test in pipeline

--- pubky-homeserver/src/core/layers/rate_limiter/layer.rs
+++ pubky-homeserver/src/core/layers/rate_limiter/layer.rs
@@ -446,6 +446,7 @@ mod tests {
         let _ = tokio::try_join!(handle1, handle2);
 
         let time_taken = start.elapsed();
+        println!("Time taken: {:?}", time_taken);
         assert!(time_taken > Duration::from_secs(3), "Should at least take 6s because uploads are limited to 1kb/s and the sum of the uploads is 6kb");
     }
 
@@ -483,6 +484,7 @@ mod tests {
         let _ = tokio::try_join!(handle1, handle2);
 
         let time_taken = start.elapsed();
+        println!("Time taken: {:?}", time_taken);
         assert!(time_taken > Duration::from_secs(3), "Should at least take 6s because downloads are limited to 1kb/s and the sum of the downloads is 6kb");
     }
 
