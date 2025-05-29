d0c6bd6e735a1b7a7dbc72bff8ca5d8af18b591e
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2025-05-29 21:15:05 +0200

    migration wip

---
+++ pubky-homeserver/src/persistence/lmdb/migrations/m290520251418_migrate_content_types.rs
@@ -0,0 +1,117 @@
+use super::super::tables::files;
+use crate::persistence::lmdb::tables::files::Entry;
+use heed::types::{Bytes, Str};
+use heed::{Database, Env, RwTxn};
+use serde::Deserialize;
+
+/// Checks if the migration is needed.
+/// Tries to read users with the new schema. If it succeeds, the migration is not needed.
+/// If it fails, the migration is needed.
+fn is_migration_needed(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<bool> {
+    let blobs: Database<Bytes, Bytes> = env
+        .open_database(wtxn, Some(files::BLOBS_TABLE))?
+        .expect("Blobs database is not available");
+
+    let metadata: Database<Str, Bytes> = env
+        .open_database(wtxn, Some(files::ENTRIES_TABLE))?
+        .expect("Entries database is not available");
+
+    let (_, meta) = metadata
+        .first(wtxn)
+        .expect("Entry table is not empty")
+        .expect("Test entry has some data");
+    let (_, file) = blobs
+        .first(wtxn)
+        .expect("Blob table is not empty")
+        .expect("Test blob has some data");
+
+    let entry = Entry::deserialize(meta).expect("Deserialization of entry failed");
+    let mime_matches = match infer::get(file) {
+        Some(kind) => kind.mime_type() == entry.content_type(),
+        None => false,
+    };
+    Ok(!mime_matches)
+}
+
+pub fn run(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<()> {
+    if !is_migration_needed(env, wtxn)? {
+        return Ok(());
+    }
+
+    tracing::info!("Running migration m290520251418_migrate_content_types");
+
+    tracing::info!("Successfully migrated");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use heed::EnvOpenOptions;
+    use pkarr::Keypair;
+    use std::io::Read;
+
+    use super::*;
+    
+    use crate::persistence::lmdb::tables::files::{Entry, InDbFileId, InDbTempFile};
+    
+    use crate::persistence::lmdb::{db::DEFAULT_MAP_SIZE, migrations::m0};
+    use crate::shared::webdav::{EntryPath, WebDavPath};
+
+    #[test]
+    fn test_is_migration_needed_yes() {
+        let tmp_dir = tempfile::tempdir().unwrap();
+        let env = unsafe {
+            EnvOpenOptions::new()
+                .max_dbs(20)
+                .map_size(DEFAULT_MAP_SIZE)
+                .open(tmp_dir.path())
+        }
+        .unwrap();
+
+        m0::run(&env, &mut env.write_txn().unwrap()).unwrap();
+        let mut wtxn = env.write_txn().unwrap();
+
+        let path = EntryPath::new(
+            Keypair::random().public_key(),
+            WebDavPath::new("/pub/foo.txt").unwrap(),
+        );
+
+        let file = tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(InDbTempFile::png_pixel())
+            .unwrap();
+
+        let mut entry = Entry::new();
+        entry.set_content_hash(*file.hash());
+        entry.set_content_length(file.len());
+        entry.set_content_type("dummy".to_string());
+        entry.set_timestamp(&Default::default());
+        let entry_key = path.to_string();
+
+        // Write a user to the old table.
+        let metadata: files::EntriesTable = env
+            .create_database(&mut wtxn, Some(files::ENTRIES_TABLE))
+            .unwrap();
+        metadata
+            .put(&mut wtxn, entry_key.as_str(), &entry.serialize())
+            .unwrap();
+
+        let blobs: files::BlobsTable = env
+            .create_database(&mut wtxn, Some(files::BLOBS_TABLE))
+            .unwrap();
+
+        let id = InDbFileId::new();
+        let mut file_handle = file.open_file_handle().unwrap();
+        let blob_key = id.get_blob_key(0);
+
+        let mut blob = vec![0_u8; 64];
+        file_handle
+            .read(&mut blob)
+            .expect("read png file successfully");
+
+        blobs.put(&mut wtxn, &blob_key, &blob[..64]).unwrap();
+
+        assert!(is_migration_needed(&env, &mut wtxn).unwrap());
+    }
+}
