6d765929b02197fba5d400a0a64ab25f04bcfe90
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2025-06-01 13:53:23 +0200

    migration test

--- pubky-homeserver/src/persistence/lmdb/migrations/m290520251418_migrate_content_types.rs
+++ pubky-homeserver/src/persistence/lmdb/migrations/m290520251418_migrate_content_types.rs
@@ -3,7 +3,6 @@ use crate::persistence::lmdb::tables::files::Entry;
 use heed::types::{Bytes, Str};
 use heed::{Database, Env, RwTxn};
 
-
 /// Checks if the migration is needed.
 /// Tries to read users with the new schema. If it succeeds, the migration is not needed.
 /// If it fails, the migration is needed.
@@ -37,7 +36,12 @@ fn is_migration_needed(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<bool> {
     }
 }
 
-fn infer_blobs_types(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<Vec<(String, String)>> {
+pub fn run(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<()> {
+    if !is_migration_needed(env, wtxn)? {
+        return Ok(());
+    }
+
+    tracing::info!("Running migration m290520251418_migrate_content_types");
     let blobs_db: Database<Bytes, Bytes> = env
         .open_database(wtxn, Some(files::BLOBS_TABLE))?
         .expect("Blobs database is not available");
@@ -46,41 +50,35 @@ fn infer_blobs_types(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<Vec<(String,
         .open_database(wtxn, Some(files::ENTRIES_TABLE))?
         .expect("Entries database is not available");
 
-    let mut blobs: Vec<(String, String)> = vec![];
+    let mut updates: Vec<(String, Vec<u8>)> = vec![];
 
-    for entry in meta_db.iter(wtxn)? {
-        if let Ok((key, data)) = entry {
-            let entry = Entry::deserialize(data).expect("Deserialization of entry failed");
-            let id = entry.file_id().get_blob_key(0);
+    for (key, data) in (meta_db.iter(wtxn)?).flatten() {
+        let mut entry = Entry::deserialize(data).expect("Deserialization of entry failed");
+        let id = entry.file_id().get_blob_key(0);
 
-            let file = blobs_db.get(wtxn, &id).expect("some").unwrap();
+        let file = blobs_db.get(wtxn, &id).expect("some").unwrap();
 
-            let mime_inferred = match infer::get(file) {
-                Some(kind) => kind.mime_type().to_string(),
-                _ => mime_guess::from_path(key)
-                    .first_or_octet_stream()
-                    .to_string(),
-            };
+        let mime_inferred = match infer::get(file) {
+            Some(kind) => kind.mime_type().to_string(),
+            _ => mime_guess::from_path(key)
+                .first_or_octet_stream()
+                .to_string(),
+        };
 
-            blobs.push((key.to_string(), mime_inferred));
-        }
+        entry.set_content_type(mime_inferred);
+        updates.push((key.to_string(), entry.serialize()));
     }
 
-    Ok(blobs)
-}
-
-pub fn run(env: &Env, wtxn: &mut RwTxn) -> anyhow::Result<()> {
-    if !is_migration_needed(env, wtxn)? {
-        return Ok(());
+    for (key, serialized_entry) in updates {
+        meta_db.put(wtxn, &key, &serialized_entry)?;
     }
-
-    tracing::info!("Running migration m290520251418_migrate_content_types");
 
     tracing::info!("Successfully migrated");
 
     Ok(())
 }
 
+#[allow(clippy::unused_io_amount)]
 #[cfg(test)]
 mod tests {
     use heed::EnvOpenOptions;
@@ -92,6 +90,7 @@ mod tests {
     use crate::persistence::lmdb::tables::files::{Entry, InDbTempFile};
 
     use crate::persistence::lmdb::{db::DEFAULT_MAP_SIZE, migrations::m0};
+
     use crate::shared::webdav::{EntryPath, WebDavPath};
 
     #[test]
@@ -147,13 +146,6 @@ mod tests {
             .expect("read png file successfully");
 
         blobs.put(&mut wtxn, &blob_key, &blob[..64]).unwrap();
-
-        // Test magic bytes inference because DB contains magic bytes file right now
-        let binding = infer_blobs_types(&env, &mut wtxn).unwrap();
-        let (_, png_test) = binding.first().unwrap();
-
-        assert_eq!(png_test, "image/png");
-
         assert!(is_migration_needed(&env, &mut wtxn).unwrap());
     }
 
@@ -210,13 +202,80 @@ mod tests {
             .expect("read png file successfully");
 
         blobs.put(&mut wtxn, &blob_key, &blob[..64]).unwrap();
-
-        // Test magic bytes inference because DB contains magic bytes file right now
-        let binding = infer_blobs_types(&env, &mut wtxn).unwrap();
-        let (_, txt_test) = binding.first().unwrap();
-
-        assert_eq!(txt_test, "text/plain");
-
         assert!(is_migration_needed(&env, &mut wtxn).unwrap());
+    }
+
+    #[test]
+    fn test_migrate() {
+        let tmp_dir = tempfile::tempdir().unwrap();
+        let env = unsafe {
+            EnvOpenOptions::new()
+                .max_dbs(20)
+                .map_size(DEFAULT_MAP_SIZE)
+                .open(tmp_dir.path())
+        }
+        .unwrap();
+        m0::run(&env, &mut env.write_txn().unwrap()).unwrap();
+        let mut wtxn = env.write_txn().unwrap();
+
+        // Write a user to the old table.
+        let path = EntryPath::new(
+            Keypair::random().public_key(),
+            WebDavPath::new("/pub/foo.txt").unwrap(),
+        );
+
+        let file = tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(InDbTempFile::png_pixel())
+            .unwrap();
+
+        let mut entry = Entry::new();
+        entry.set_content_hash(*file.hash());
+        entry.set_content_length(file.len());
+        entry.set_content_type("dummy".to_string());
+        entry.set_timestamp(&Default::default());
+        let entry_key = path.to_string();
+
+        // Write a file.
+        let metadata: files::EntriesTable = env
+            .create_database(&mut wtxn, Some(files::ENTRIES_TABLE))
+            .unwrap();
+
+        metadata
+            .put(&mut wtxn, entry_key.as_str(), &entry.serialize())
+            .unwrap();
+
+        let blobs: files::BlobsTable = env
+            .create_database(&mut wtxn, Some(files::BLOBS_TABLE))
+            .unwrap();
+
+        let blob_key = entry.file_id().get_blob_key(0);
+        let mut blob = vec![0_u8; 64];
+
+        let mut file_handle = file.open_file_handle().unwrap();
+
+        file_handle
+            .read(&mut blob)
+            .expect("read png file successfully");
+
+        blobs.put(&mut wtxn, &blob_key, &blob[..64]).unwrap();
+
+        // Migrate content type
+        run(&env, &mut wtxn).unwrap();
+
+        // Check that the content type has changed according to magic bytes of the file
+        let metadata: Database<Str, Bytes> = env
+            .open_database(&wtxn, Some(files::ENTRIES_TABLE))
+            .unwrap()
+            .expect("Entries database is not available");
+
+        let (_, meta) = metadata.first(&wtxn).unwrap().unwrap();
+        let entry = Entry::deserialize(meta).expect("Deserialization of entry failed");
+
+        assert_eq!(
+            entry.content_type(),
+            "image/png",
+            "The content type should be updated."
+        );
     }
 }
