072ff9e57fa1cee77dfff135da0d759447bd0e2b
dzdidi <dzdidi@protonmail.com>
2025-08-29 11:30:14 +0200

    todos markings on path and route handling
    Signed-off-by: dzdidi <dzdidi@protonmail.com>

--- pubky-homeserver/src/core/routes/tenants/mod.rs
+++ pubky-homeserver/src/core/routes/tenants/mod.rs
@@ -14,6 +14,9 @@ pub mod write;
 pub fn router(state: AppState) -> Router<AppState> {
     Router::new()
         .route("/session", get(session::session).delete(session::signout))
+        // XXX: dzdidi new path example:
+        // https://qtnyghnq9swketdtj9drc7rs5pfnxhs61gq4jwd317ezdegcrbco/dav/qtnyghnq9swketdtj9drc7rs5pfnxhs61gq4jwd317ezdegcrbco/pub/test.txt
+        // via https://github.com/pubky/pubky-core/pull/145#discussion_r2149297326
         .route(
             "/{*path}",
             get(read::get)
@@ -23,5 +26,7 @@ pub fn router(state: AppState) -> Router<AppState> {
         )
         // TODO: different max size for sessions and other routes?
         .layer(DefaultBodyLimit::max(100 * 1024 * 1024))
+        // XXX: dzdidi - WebDAV compliant auth. Which is actually http basic auth so we need some magic here
+        // to make session based auth look like http auth while also accepting http basic auth for webDAV comp
         .layer(AuthorizationLayer::new(state.clone()))
 }
