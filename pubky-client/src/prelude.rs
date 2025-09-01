d1700c19bc3f5f7c1a2b4fdbc40dc9e7106e1e5d
SHAcollision <cotyleron@proton.me>
2025-09-01 13:00:52 +0900

    update prelude

--- pubky-client/src/prelude.rs
+++ pubky-client/src/prelude.rs
@@ -6,16 +6,23 @@ pub use crate::{BuildError, Error, Keypair, PublicKey};
 // Transport
 pub use crate::{PubkyClient, PubkyClientBuilder};
 
-// Agent to use on behalf of a user
+// High level Actors
+// Agent to use on behalf of a user on apps.
 pub use crate::PubkyAgent;
+// Signer to use on behalf of a user on a keychain application.
+pub use crate::PubkySigner;
+// Authentication flow for apps.
+pub use crate::PubkyAuth;
+// Homeserver drive storage API (http verbs + list)
+pub use crate::PubkyDrive;
+// Pkdns/Pkarr retrieval and publishing
+pub use crate::PkDns;
+
+// Helpers
 // Homeserver Paths / URLs
-pub use crate::{FilePath, PubkyPath};
-
-// helpers:
-// pub use crate::{ListBuilder, Session};
-
-// Capabilities for auth flows
+pub use crate::{FilePath, ListBuilder, PubkyPath};
+// Session and  capabilities for auth flows
+pub use crate::Session;
 pub use crate::{Capabilities, Capability};
-
-// Recovery utilities
+// Secret recovery utilities
 pub use crate::recovery_file;
