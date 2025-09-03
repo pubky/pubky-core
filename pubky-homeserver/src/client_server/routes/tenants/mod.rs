b1b3d55b2ef1139fb63b41a93d2ab3e0cca86b9c
dzdidi <dzdidi@protonmail.com>
2025-09-03 08:52:35 +0200

    extend app state with inner open dav handler
    Signed-off-by: dzdidi <dzdidi@protonmail.com>

--- pubky-homeserver/src/client_server/routes/tenants/mod.rs
+++ pubky-homeserver/src/client_server/routes/tenants/mod.rs
@@ -30,18 +30,27 @@ pub fn router(state: AppState) -> Router<AppState> {
 }
 
 pub fn webdav_router(state: AppState) -> Router<AppState> {
-    // TODO: layers for:
-    // - webdav auth (http basic)
-    // - anything else?
     Router::new()
         .route(
             "/{key}/{*path}",
+            // FIXME use state.inner_dav_handler.handle
             get(read::get)
                 .head(read::head)
                 .put(write::put)
                 .delete(write::delete),
         )
         .layer(DefaultBodyLimit::max(100 * 1024 * 1024))
-        // FIXME
+        // NOTE observed that admin's dav auth is managed by `routes::dav_handler.rs`
+        // For now I think it is better to keep it in Layer
+        // TODO: layers for webdav supported auth:
+        // - Basic Authentication https://www.rfc-editor.org/rfc/rfc7617
+        //   who needs passwords when we have keys?
+        // - OAuth2.0 Authentication https://www.rfc-editor.org/rfc/rfc6749
+        //   We should explore how OAuth flow can be mapped on Pubky statck.
+        // - Digest Authentication https://www.rfc-editor.org/rfc/rfc7616
+        //   This is basically how current auth with session works, check if this is a good start
+        // - Client Certificates https://www.rfc-editor.org/rfc/rfc8705
+        //   This is the way to go in the long run
+        // Current PubkyAuth can be found here https://github.com/pubky/pubky-core/blob/main/docs/src/spec/auth.md
         .layer(AuthorizationLayer::new(state.clone()))
 }
