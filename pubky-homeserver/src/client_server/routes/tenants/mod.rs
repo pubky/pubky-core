f04918ffcdfb276acce07eba243d998f268634d9
dzdidi <dzdidi@protonmail.com>
2025-09-03 16:51:55 +0200

    draft hack api
    Signed-off-by: dzdidi <dzdidi@protonmail.com>

--- pubky-homeserver/src/client_server/routes/tenants/mod.rs
+++ pubky-homeserver/src/client_server/routes/tenants/mod.rs
@@ -40,27 +40,40 @@ pub fn webdav_router(state: AppState) -> Router<AppState> {
     Router::new()
         .route("/{key}/{*path}", any(dav_handler))
         .layer(DefaultBodyLimit::max(100 * 1024 * 1024))
-        // NOTE observed that admin's dav auth is managed by `routes::dav_handler.rs`
-        // For now I think it is better to keep it in Layer
-        // TODO: layers for webdav supported auth:
-        // - Basic Authentication https://www.rfc-editor.org/rfc/rfc7617
-        //   who needs passwords when we have keys?
-        // - OAuth2.0 Authentication https://www.rfc-editor.org/rfc/rfc6749
-        //   We should explore how OAuth flow can be mapped on Pubky statck.
-        // - Digest Authentication https://www.rfc-editor.org/rfc/rfc7616
-        //   This is basically how current auth with session works, check if this is a good start
-        // - Client Certificates https://www.rfc-editor.org/rfc/rfc8705
-        //   This is the way to go in the long run
-        // Current PubkyAuth can be found here https://github.com/pubky/pubky-core/blob/main/docs/src/spec/auth.md
-        .layer(AuthorizationLayer::new(state.clone()))
+    // NOTE observed that admin's dav auth is managed by `routes::dav_handler.rs`
+    // For now I think it is better to keep it in Layer
+    // TODO: layers for webdav supported auth:
+    // - Basic Authentication https://www.rfc-editor.org/rfc/rfc7617
+    //   who needs passwords when we have keys?
+    // - OAuth2.0 Authentication https://www.rfc-editor.org/rfc/rfc6749
+    //   We should explore how OAuth flow can be mapped on Pubky statck.
+    // - Digest Authentication https://www.rfc-editor.org/rfc/rfc7616
+    //   This is basically how current auth with session works, check if this is a good start
+    // - Client Certificates https://www.rfc-editor.org/rfc/rfc8705
+    //   This is the way to go in the long run
+    // Current PubkyAuth can be found here https://github.com/pubky/pubky-core/blob/main/docs/src/spec/auth.md
+    //
+    // .layer(AuthorizationLayer::new(state.clone()))
 }
 
 pub async fn dav_handler(
     State(state): State<AppState>,
-    Path((_key, _path)): Path<(Z32Pubkey, String)>,
+    Path((key, path)): Path<(Z32Pubkey, String)>,
     req: Request<Body>,
 ) -> HttpResult<impl IntoResponse> {
     // TODO: handle pubky (part of path) somehow
+
+    let (mut parts, body) = req.into_parts();
+
+    let new_path = parts
+        .uri
+        .to_string()
+        .replacen(&format!("/{}", key.0.to_string()), "", 1);
+    let new_uri = new_path.parse().unwrap();
+    parts.uri = new_uri;
+
+    let req = Request::from_parts(parts, body);
+
     let dav_response = state.inner_dav_handler.handle(req).await;
     Ok(dav_response.into_response())
 }
