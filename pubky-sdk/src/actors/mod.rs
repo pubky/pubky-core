8c1a49c2a913a8de213cfaa3818b2185d1ee93cd
Severin Buhler <sevi_buehler@hotmail.com>
2025-11-21 17:07:59 +0100

    moved constant

--- pubky-sdk/src/actors/mod.rs
+++ pubky-sdk/src/actors/mod.rs
@@ -1,7 +1,6 @@
 pub mod auth_flow;
 mod http_relay_link_channel;
 mod auth_permission_subscription;
-mod constants;
 pub mod pkdns;
 mod session;
 mod signer;
@@ -9,7 +8,7 @@ pub mod signup_auth_flow;
 pub mod storage;
 
 pub use auth_flow::PubkyAuthFlow;
-pub use constants::DEFAULT_HTTP_RELAY;
+pub use http_relay_link_channel::DEFAULT_HTTP_RELAY;
 pub use pkdns::Pkdns;
 pub use session::core::PubkySession;
 pub use signer::PubkySigner;
